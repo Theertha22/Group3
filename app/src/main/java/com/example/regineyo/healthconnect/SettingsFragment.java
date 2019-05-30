72bc312402e7d05b65d52e4cd06d8bfbf758a19e
Ishita Verghese <ishitaverghese@gmail.com>
2019-05-30 18:39:11 +1000

    made changes to viewing Settings - SettingsFragment

--- app/src/main/java/com/example/regineyo/healthconnect/SettingsFragment.java
+++ app/src/main/java/com/example/regineyo/healthconnect/SettingsFragment.java
@@ -27,31 +27,14 @@ public class SettingsFragment extends Fragment {
      * @return A new instance of fragment SettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-//    public static SettingsFragment newInstance(String param1, String param2) {
-//        SettingsFragment fragment = new SettingsFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        View viewSettings = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        view.findViewById(R.id.logoutBtn).setOnClickListener(new View.OnClickListener() {
+        viewSettings.findViewById(R.id.logoutBtn).setOnClickListener(new View.OnClickListener() {
             @Override
                 public void onClick(View v) {
                     FirebaseAuth.getInstance().signOut();
@@ -60,7 +43,7 @@ public class SettingsFragment extends Fragment {
             }
         });
 
-        return view;
+        return viewSettings;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
